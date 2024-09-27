aa041c53bf15061432838ea0b61a099ff82a470c
Alexander Emelyanov <alalemelianov@edu.hse.ru>
2024-09-27 16:22:39 +0300

    Added matrix input

--- main.cpp
+++ main.cpp
@@ -1,56 +1,54 @@
 #include <iostream>
 #include "LinearEquationSystem.h"
 
-using namespace std;
-
 int main() {
-    srand(time(0));
-    int N = 20;
-    Matrix<int> A(N, N), B(N, 1);
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            A(i, j) = rand() % 100;
+    int N;
+    std::cin >> N;
+    Matrix<Large> A(N, N), B(N, 1);
+    for(int i = 0; i < N; ++i) {
+        for(int j = 0; j < N; ++j) {
+            std::cin >> A(i, j);
         }
-        B(i, 0) = rand() % 100;
+        std::cin >> B(i, 0);
     }
     LinearEquationSystem sys = {A, B};
     sys.Solve();
-    cout << sys << endl << endl;
+    std::cout << sys << std::endl << std::endl;
 
     auto solutions = sys.GetSolutions();
     if (solutions.empty()) {
-        cout << "No solutions";
+        std::cout << "No solutions";
         return 0;
     }
     for (const auto &solution: solutions) {
         if (solution.variable.coeff == -1) {
-            cout << "-";
+            std::cout << "-";
         } else if (solution.variable.coeff != 1) {
-            cout << solution.variable.coeff << "*";
+            std::cout << solution.variable.coeff << "*";
         }
-        cout << "x_" << solution.variable.var_index << " = ";
+        std::cout << "x_" << solution.variable.var_index << " = ";
 
         bool is_first = true;
         for (const auto &var: solution.expression) {
             if (is_first && var.coeff < 0) {
-                cout << "-";
+                std::cout << "-";
             } else if (!is_first && var.coeff < 0) {
-                cout << " - ";
+                std::cout << " - ";
             } else if (!is_first && var.coeff > 0) {
-                cout << " + ";
+                std::cout << " + ";
             }
 
             if (var.var_index == 0) {
-                cout << abs(var.coeff);
+                std::cout << abs(var.coeff);
                 continue;
             }
 
             if (abs(var.coeff) != 1) {
-                cout << abs(var.coeff) << "*";
+                std::cout << abs(var.coeff) << "*";
             }
-            cout << "x_" << var.var_index;
+            std::cout << "x_" << var.var_index;
             is_first = false;
         }
-        cout << endl;
+        std::cout << std::endl;
     }
 }
